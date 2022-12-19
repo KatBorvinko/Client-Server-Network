9ec3c021f8b0ddfda4c4e1dc5a64640c3b014d8a
CT673 <108407338+CT673@users.noreply.github.com>
2022-12-20 00:10:31 +1000

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,43 +1,4 @@
-import json
-import pickle
-import dict2xml as dict2xml
-import Config
-import socket
-from dict2xml import dict2xml
-from cryptography.fernet import Fernet
+# Connects with the server and prints the different existing user options according with the dictionary
+import Server
+Countries = Server.payload
 
-COUNTRIES = Config.countries
-
-s = socket.socket()
-client = socket.socket()
-client.connect((Config.server_address, Config.port_number))
-
-
-# Serialization/ pickle file
-def serialise_dictionary(serialization):
-    if Config.serialization_option == "JSON":
-        return json.dumps(serialization)
-
-    elif Config.serialization_option == "BINARY":
-        return pickle.dumps(serialization)
-
-    elif Config.serialization_option == "XML":
-        return dict2xml(serialization, wrap="dictionary", indent="  ")
-
-    else:
-        print("format not recognised")
-
-
-payload = serialise_dictionary(COUNTRIES)
-client.send(payload.encode('utf8'))
-
-with open('GrpC.txt', 'w') as data:
-    data.write(str(COUNTRIES))
-
-
-# File encryption
-def encrypt():
-    f = Fernet(Config.key)
-    text = input("GrpC.txt").encode()
-    encrypted = f.encrypt(text)
-    print("Encrypted dictionary")
